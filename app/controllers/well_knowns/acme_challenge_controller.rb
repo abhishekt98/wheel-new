0963af0317596493fb9836006700200dd34d382e
Abhishek T <47141466+abhishekt98@users.noreply.github.com>
2024-10-10 19:11:47 +0530

    Update acme_challenge_controller.rb

--- app/controllers/well_knowns/acme_challenge_controller.rb
+++ app/controllers/well_knowns/acme_challenge_controller.rb
@@ -4,6 +4,6 @@ class WellKnowns::AcmeChallengeController < ApplicationController
   protect_from_forgery with: :null_session
 
   def show
-    redirect_to("https://wheel-test-3.neetodeployapp.com/.well-known/acme-challenge/token",allow_other_host: true)
+    redirect_to("http://wheel-test-3.neetodeployapp.com/.well-known/acme-challenge/token",allow_other_host: true)
   end
 end
